6b77ae6c48ff50548cece91252492bbc89d7950c
“isabela.araujo <I.kuser@edu.pucrs.br>
2025-09-15 22:07:59 -0300

    ajustando test file

--- scripts/generate-test-file.sh
+++ scripts/generate-test-file.sh
@@ -7,7 +7,7 @@ echo "Generating 200MB test file..."
 mkdir -p test-files
 
 # Generate 200MB file with random data
-dd if=/dev/urandom of=test-files/test-200mb.bin bs=1M count=200
+dd if=/dev/urandom of=test-files/test_200MB.bin bs=1M count=200
 
-echo "Test file generated: test-files/test-200mb.bin (200MB)"
-ls -lh test-files/test-200mb.bin
+echo "Test file generated: test-files/test_200MB.bin (200MB)"
+ls -lh test-files/test_200MB.bin
